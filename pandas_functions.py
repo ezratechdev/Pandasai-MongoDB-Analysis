abf4ff2609e21cb273fde49f1c8f64588eb124f6
ezratechdev <254gtgt@gmail.com>
2025-09-25 09:33:02 +0300

    Made more analytics on customer with the most purchases and how much they spent on average

--- pandas_functions.py
+++ pandas_functions.py
@@ -1,8 +1,6 @@
 from main import df
 import pandas as pd
 
-print(df)
-
 columns = df.columns
 
 if len(columns) == 0:
@@ -14,11 +12,24 @@ if not target_column in columns: #For my case I am targetting amount, be free to
     raise ValueError("The amount column was not found")
 
 
-valid = (df['isUsed'] == True | df['transaction_ref'].notnull())
-mean = df.loc[valid, target_column].mean()
-
 # I am using query to set conditions for the rows I would like to target
-total = df.loc[valid, target_column].sum()
+valid = (df['isUsed'] == True | df['transaction_ref'].notnull())
+amount_data = df.loc[valid, target_column]
+mean = amount_data.mean()
 
-print(f"The amount avarage is {mean}\nThe amount total is {total}")
+total = amount_data.sum()
+
+meadian = amount_data.median()
+
+'''
+Broke the convention here as I needed more analysis.
+I did not stick to the name hardcoded to the target_column variable and started exploring data as per interes
+'''
+all_time_customer = df.loc[valid, 'name'].value_counts().idxmax()
+
+all_time_customer_valid_offer = (df['name'] == all_time_customer)
+
+all_time_customer_favorite = df.loc[all_time_customer_valid_offer, target_column].mean()
+
+print(f"The amount avarage is {mean}\nThe amount total is {total}\nThe median amount is {meadian}\nCustomer with most purchases is {all_time_customer}\n{all_time_customer} made an average purchase of {all_time_customer_favorite}")
 
